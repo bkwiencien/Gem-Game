cccc6d866e58797f1946480f4a7ceba4caa399cc
Bob Kwiencien <bkwiencien@gmail.com>
2017-08-01 15:18:39 -0400

    Aug1-15:18

--- gemgame.js
+++ gemgame.js
@@ -4,6 +4,18 @@ var gem1 = {
    name: "gem1",
    value: 0
 }
+var gem2 = {
+   name: "gem1",
+   value: 0
+}
+var gem3 = {
+   name: "gem1",
+   value: 0
+}
+var gem4 = {
+   name: "gem1",
+   value: 0
+}
 function initialize() {
  console.log("i started");
  targetScore = 0;
