d402aef439d0d41bcd19fe3fdc7aa70db07b9e53
Bob Kwiencien <bkwiencien@gmail.com>
2017-08-03 11:39:35 -0400

    Aug3-11:39

--- gemgame1.js
+++ gemgame1.js
@@ -18,6 +18,7 @@ var gem4 = {
    name: "gem4",
    value: 0
 }
+var gemArray = [gem1,gem2,gem3,gem4];
 function initialize() {
  targetScore = 0;
  currentScore = 0;
@@ -26,38 +27,21 @@ $("#totalscore").html("Your total score is " + currentScore);
  gem2.value = 0;
  gem3.value = 0;
  gem4.value = 0;
- setUpGem1();
- setUpGem2();
- setUpGem3();
- setUpGem4();
+ setUpAllGems();
  resetYourScore();
  generateTargetScore();
 }
-function setUpGem1() {
-// Each crystal should have a random hidden value between 1 - 12.
- var tempnum = Math.round(Math.random()*10);
- if (tempnum == 0) {
-   tempnum = 1;
- }
- gem1.value = tempnum  % 13;
- console.log("gem1 has a value of " + gem1.value);
-}
-function setUpGem2() {
- var tempnum = Math.round(Math.random()*10);
- gem2.value = tempnum  % 13;
- console.log("gem2 has a value of " + gem2.value);
-}
-function setUpGem3() {
- var tempnum = Math.round(Math.random()*10);
- gem3.value = tempnum  % 13;
- console.log("gem3 has a value of " + gem3.value);
-}
-function setUpGem4() {
- var tempnum = Math.round(Math.random()*10);
- gem4.value = tempnum  % 13;
- console.log("gem4 has a value of " + gem4.value);
-}
+function setUpAllGems() {
+  for (i=0;i<gemArray.length;i++) {
+    var tempnum = Math.round(Math.random()*10);
+    if (tempnum == 0) {
+      tempnum = 1;
+     }
+     gemArray[i].value = tempnum  % 13;
+     console.log(gemArray[i].name + "  has a value of " + gemArray[i].value);
 
+  }
+}
 function generateTargetScore() {
 // The random number shown at the start of the game should be between 19 - 120.
 // $("#random-number").html(rnd);
